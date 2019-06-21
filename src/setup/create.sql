88b153f0792650b9273e0ad14cd1075c4318e7a0
FAUCHER ELLIOT p1811851 <p1811851@univ-lyon1.fr>
2019-06-21 14:27:22 +0200

    Fin du SQL

--- src/setup/create.sql
+++ src/setup/create.sql
@@ -1,53 +1,64 @@
 create table bank.agence(
-    idAgence  		long     		NOT NULL        PRIMARY KEY,
+    idAgence  		int     		PRIMARY KEY		NOT NULL,
+    nom		  		varchar(100)    NOT NULL,
     horaires  		varchar(500)    NOT NULL,
     telephone 		varchar(10)     NOT NULL,
     adresse   		varchar(300)    NOT NULL
 );
 
-create table bank.utilisateur(
-	idUtilisateur 	long     	  	NOT NULL        PRIMARY KEY,
-	login     		varchar(20)     NOT NULL		UNIQUE,
+create table bank.client(
+	idClient  		int     		PRIMARY KEY		NOT NULL,
+	login     		varchar(20)     UNIQUE			NOT NULL,
     pwd       		varchar(30)     NOT NULL,
 	nom       		varchar(30)     NOT NULL,
 	prenom    		varchar(30)     NOT NULL,
+	adresse      	varchar(300)    NOT NULL,
 	telephone 		varchar(10)     NOT NULL,
-	mail      		varchar(100)    NOT NULL
-);
-
-create table bank.client(
-	idClient  		long     		NOT NULL        PRIMARY KEY
+	mail      		varchar(100)    NOT NULL,
+	idConseiller 	int     	  					NOT NULL,
+	idAgence 	int     	  					NOT NULL
 );
 
 create table bank.conseiller(
-	idConseiller 	long     	  	NOT NULL        PRIMARY KEY
+	idConseiller 	int     	  	PRIMARY KEY		NOT NULL,
+	login     		varchar(20)     UNIQUE			NOT NULL,
+    pwd       		varchar(30)     NOT NULL,
+	nom       		varchar(30)     NOT NULL,
+	prenom    		varchar(30)     NOT NULL,
+	adresse      	varchar(300)    NOT NULL,
+	telephone 		varchar(10)     NOT NULL,
+	mail      		varchar(100)    NOT NULL,
+	idAgence 	int     	  					NOT NULL
 );
 
 create table bank.message(
-	idMessage 		long     		NOT NULL        PRIMARY KEY,
+	idMessage 		int     		PRIMARY KEY		NOT NULL,
+	idClient 		int     	  					NOT NULL,
     date      		date           	NOT NULL,
     contenu   		varchar(500)   	NOT NULL
 );
 
 create table bank.compte(
-    iban      		varchar(50)     NOT NULL        PRIMARY KEY,
+    iban      		varchar(50)     PRIMARY KEY		NOT NULL,
     solde     		double     	  	NOT NULL
 );
 
 create table bank.operation(
-    idOperation 	long           	NOT NULL        PRIMARY KEY,
-    date      		date          	NOT NULL,
-	montant   		double     	  	NOT NULL
+    idOperation 		int           	PRIMARY KEY		NOT NULL,
+    ibanSource		 	varchar(50),
+    ibanDestinataire 	varchar(50),
+    date      			date          	NOT NULL,
+	montant   			double     	  	NOT NULL
 );
 
 create table bank.clientCompte(
-    idClient  		long         	NOT NULL,
-    idCompte  		long          	NOT NULL
+    idClient  		int         	NOT NULL,
+    ibanCompte  		varchar(50)          	NOT NULL
 );
 
 ALTER TABLE bank.clientCompte 
 ADD CONSTRAINT PK_ClientCompte 
-PRIMARY KEY (idClient, idCompte);
+PRIMARY KEY (idClient, ibanCompte);
 
 ALTER TABLE bank.conseiller
 ADD CONSTRAINT FK_ConseillerAgence
@@ -71,12 +82,16 @@ FOREIGN KEY (idClient) REFERENCES bank.client(idClient);
 
 ALTER TABLE bank.clientCompte
 ADD CONSTRAINT FK_ClientcompteCompte
-FOREIGN KEY (idCompte) REFERENCES bank.compte(idCompte);
+FOREIGN KEY (ibanCompte) REFERENCES bank.compte(iban);
 
 ALTER TABLE bank.operation
 ADD CONSTRAINT FK_OperationCompteIbSrc
 FOREIGN KEY (ibanSource) REFERENCES bank.compte(iban);
 
 ALTER TABLE bank.operation
-ADD CONSTRAINT FK_OperationComptIbDest
+ADD CONSTRAINT FK_OperationDestinataire
 FOREIGN KEY (ibanDestinataire) REFERENCES bank.compte(iban);
+
+ALTER TABLE bank.operation
+ADD CONSTRAINT FK_OperationSource
+FOREIGN KEY (ibanSource) REFERENCES bank.compte(iban);
